bec32efc230ea02136e07c7c66d62e2ef0a6316c
T.ann <anhlt@dipro.vn>
2025-04-16 22:32:32 +0700

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,25 @@ module.exports = {
 	env: {
 		browser: true,
 		commonjs: true,
-		es2021: true,
+		es2021: true
 	},
-	extends: "standard",
+	extends: 'standard',
 	overrides: [
 		{
 			env: {
-				node: true,
+				node: true
 			},
-			files: [".eslintrc.{js,cjs}"],
+			files: [
+				'.eslintrc.{js,cjs}'
+			],
 			parserOptions: {
-				sourceType: "script",
-			},
-		},
+				sourceType: 'script'
+			}
+		}
 	],
 	parserOptions: {
-		ecmaVersion: "latest",
+		ecmaVersion: 'latest'
 	},
 	rules: {
-		quotes: ["off"],           // Tắt rule bắt buộc dùng single quote
-		semi: ["off"],             // Tắt rule cấm dùng dấu chấm phẩy
-		"no-unused-vars": ["warn"], // Không báo lỗi, chỉ cảnh báo nếu biến không dùng
-		"spaced-comment": ["off"], // Cho phép comment không có dấu cách
-	},
-};
+	}
+}
